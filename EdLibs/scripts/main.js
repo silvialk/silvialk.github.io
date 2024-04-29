8c1b72465ebef13df4d3300a3928062b66a05e6e
Brian Grossman <bgrossman@alum.mit.edu>
2024-04-28 20:34:14 -0400

    Update main.js

--- EdLibs/scripts/main.js
+++ EdLibs/scripts/main.js
@@ -22,7 +22,9 @@ drop_handler = function(ev) {
     currDragItem.style.position    = 'absolute';
     currDragItem.style.left        = ev.clientX - left - offsetX + 'px';
     currDragItem.style.top         = ev.clientY - top - offsetY + 'px';
-    drop_zone_elt.appendChild(currDragItem);
+    
+    drop_zone_elt.appendChild(currDragItem.cloneNode(true));
+    // drop_zone_elt.appendChild(currDragItem);
 };
 
 dragover_handler = function(ev) {
